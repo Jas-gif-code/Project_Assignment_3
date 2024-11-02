26b4c09668bb8b8d70220895d94afd0c1f1214ca
Jas-gif-code <Jboparai7647@conestogac.on.ca>
2024-11-02 01:05:00 -0400

    Fixed test error

--- Project_Assignment_3/rps.c
+++ Project_Assignment_3/rps.c
@@ -1,11 +1,10 @@
 #include <string.h>
 
 static const char* determine_winner(const char* player1, const char* player2) {
-    if (strcmp(player1, player2) == 0) {
-        return "Draw";
-    }
-    else {
-        return "Player1"; 
+    // Check for invalid inputs
+    if ((strcmp(player1, "Rock") != 0 && strcmp(player1, "Paper") != 0 && strcmp(player1, "Scissors") != 0) ||
+        (strcmp(player2, "Rock") != 0 && strcmp(player2, "Paper") != 0 && strcmp(player2, "Scissors") != 0)) {
+        return "Invalid";
     }
     
 
